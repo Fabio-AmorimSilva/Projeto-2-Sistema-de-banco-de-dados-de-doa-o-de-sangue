e271790ddd8d5795a5e985b8140786beaf24e738
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2024-12-31 19:51:44 -0300

    chore: Removing unused namespaces

--- BloodDonationManagement.Application/GlobalUsings.cs
+++ BloodDonationManagement.Application/GlobalUsings.cs
@@ -1,3 +1,8 @@
 ﻿// Global using directives
 
+global using BloodDonationManagement.Application.Dtos;
+global using BloodDonationManagement.Domain.Enums;
+global using BloodDonationManagement.Domain.Messages;
+global using FluentValidation;
+global using MediatR;
 global using Microsoft.Extensions.DependencyInjection;
