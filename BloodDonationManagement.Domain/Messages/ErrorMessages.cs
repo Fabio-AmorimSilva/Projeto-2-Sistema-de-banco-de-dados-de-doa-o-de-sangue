9131b3e4e1f9176b26bd345c9311c9477e6e2c8e
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2024-12-31 19:51:10 -0300

    feat: Creating error messages

---
+++ BloodDonationManagement.Domain/Messages/ErrorMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace BloodDonationManagement.Domain.Messages;
+
+public static class ErrorMessages
+{
+    public static string CannotBeEmpty(string field)
+        => $"{field} cannot be empty.";
+    
+    public static string NotFound<T>()
+        => $"{typeof(T).Name} not found.";
+}
