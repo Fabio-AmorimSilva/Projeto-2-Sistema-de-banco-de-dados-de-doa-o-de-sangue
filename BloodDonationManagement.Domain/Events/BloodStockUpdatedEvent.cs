9c2f618143d90d703ebd8b80344ebfc62a91c4c9
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-01 19:50:29 -0300

    feat: Creating and implementing event and handler for blood stock updated

---
+++ BloodDonationManagement.Domain/Events/BloodStockUpdatedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace BloodDonationManagement.Domain.Events;
+
+public class BloodStockUpdatedEvent : Event
+{
+    public BloodType BloodType { get; set; }
+    public RhFactor RhFactor { get; set; }
+    public int Quantity { get; set; }
+}
