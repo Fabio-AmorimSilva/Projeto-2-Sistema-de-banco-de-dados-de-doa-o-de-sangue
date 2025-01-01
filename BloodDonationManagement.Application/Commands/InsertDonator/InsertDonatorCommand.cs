2eb3b4c196f349cec97c11cbded1b38cb816e5b6
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-01 19:48:46 -0300

    refactor: Implementation of command and handler

--- BloodDonationManagement.Application/Commands/InsertDonator/InsertDonatorCommand.cs
+++ BloodDonationManagement.Application/Commands/InsertDonator/InsertDonatorCommand.cs
@@ -1,25 +1,54 @@
 ﻿namespace BloodDonationManagement.Application.Commands.InsertDonator;
 
 public record InsertDonatorCommand(
-    string DonatorId,
-    DateTime DonationDate,
-    string Quantity
+    string Name,
+    string Email,
+    DateTime Birth,
+    Gender Gender,
+    double Weight,
+    BloodType BloodType,
+    RhFactor RhFactor,
+    string Cep
 ) : IRequest;
 
-public class InsertDonatorCommandValidator : AbstractValidator<InsertDonatorCommand>
+public class InsertDonationCommandValidator : AbstractValidator<InsertDonatorCommand>
 {
-    public InsertDonatorCommandValidator()
+    public InsertDonationCommandValidator()
     {
-        RuleFor(command => command.DonatorId)
+        RuleFor(command => command.Name)
             .NotEmpty()
-            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(InsertDonatorCommand.DonatorId)));
-        
-        RuleFor(command => command.DonationDate)
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(InsertDonatorCommand.Name)));
+
+        RuleFor(command => command.Email)
             .NotEmpty()
-            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(InsertDonatorCommand.DonationDate)));
-        
-        RuleFor(command => command.Quantity)
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(InsertDonatorCommand.Email)));
+
+        RuleFor(command => command.Birth)
             .NotEmpty()
-            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(InsertDonatorCommand.Quantity)));
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(InsertDonatorCommand.Birth)));
+
+        RuleFor(command => command.Gender)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(InsertDonatorCommand.Gender)));
+
+        RuleFor(command => command.Weight)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(InsertDonatorCommand.Weight)));
+
+        RuleFor(command => command.BloodType)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(InsertDonatorCommand.BloodType)));
+
+        RuleFor(command => command.RhFactor)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(InsertDonatorCommand.RhFactor)));
+
+        RuleFor(command => command.Name)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(InsertDonatorCommand.Name)));
+
+        RuleFor(command => command.Cep)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(InsertDonatorCommand.Cep)));
     }
 }
--- BloodDonationManagement.Application/Commands/InsertDonator/InsertDonatorCommandHandler.cs
+++ BloodDonationManagement.Application/Commands/InsertDonator/InsertDonatorCommandHandler.cs
@@ -1,6 +1,32 @@
 ﻿namespace BloodDonationManagement.Application.Commands.InsertDonator;
 
-public class InsertDonatorCommandHandler
+public class InsertDonatorCommandHandler(
+    IDonatorRepository repository,
+    IUnitOfWork unitOfWork,
+    GetAddressViaCepService service
+) : IRequestHandler<InsertDonatorCommand>
 {
-    
+    public async Task Handle(InsertDonatorCommand request, CancellationToken cancellationToken)
+    {
+        var address = await service.GetAddressViaCep(request.Cep);
+
+        var donator = new Donator(
+            name: request.Name,
+            email: request.Email,
+            birth: request.Birth,
+            gender: request.Gender,
+            weight: request.Weight,
+            bloodType: request.BloodType,
+            rhFactor: request.RhFactor,
+            address: new Address(
+                publicArea: address.Data!.PublicArea,
+                city: address.Data.City,
+                state: address.Data.State,
+                cep: address.Data.Cep
+            )
+        );
+
+        await repository.AddAsync(donator);
+        await unitOfWork.Commit();
+    }
 }
