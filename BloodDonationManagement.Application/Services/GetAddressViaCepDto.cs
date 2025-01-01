da9db31df8955f3c67300640ecf6bc3870baef7a
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-01 19:48:14 -0300

    feat: Creating service to retrieve address

---
+++ BloodDonationManagement.Application/Services/GetAddressViaCepDto.cs
@@ -0,0 +1,18 @@
+﻿namespace BloodDonationManagement.Application.Services;
+
+public record GetAddressViaCepDto
+{
+    public string Cep { get; init; } = null!;
+    public string Logradouro { get; init; } = null!;
+    public string Complemento { get; init; } = null!;
+    public string Unidade { get; init; } = null!;
+    public string Bairro { get; init; } = null!;
+    public string Localidade { get; init; } = null!;
+    public string Uf { get; init; } = null!;
+    public string Estado { get; init; } = null!;
+    public string Regiao { get; init; } = null!;
+    public string Ibge { get; init; } = null!;
+    public string Gia { get; init; } = null!;
+    public string DDD { get; init; } = null!;
+    public string Siafi { get; init; } = null!;
+}
---
+++ BloodDonationManagement.Application/Services/GetAddressViaCepService.cs
@@ -0,0 +1,24 @@
+﻿namespace BloodDonationManagement.Application.Services;
+
+public class GetAddressViaCepService
+{
+    public async Task<ResultDto<ResponseAddressDto>> GetAddressViaCep(string cep)
+    {
+        var httpClient = new HttpClient();
+        
+        var response = await httpClient.GetFromJsonAsync<GetAddressViaCepDto>(requestUri: $"https://viacep.com.br/ws/{cep}/json/");
+
+        if (response is null)
+            return ResultDto<ResponseAddressDto>.Error(ErrorMessages.NotFound<Address>());
+        
+        var address = new ResponseAddressDto
+        {
+            PublicArea = response.Logradouro,
+            Cep = response.Cep,
+            City = response.Localidade,
+            State = response.Estado
+        };
+        
+        return new ResultDto<ResponseAddressDto>(address);
+    }
+}
