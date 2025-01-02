a32f0f444a1fb7c42691f470b15b264ebb091936
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-02 19:54:14 -0300

    refactor: Implementing repositories and unit of work in service collection extensions on infrastructure layer

--- BloodDonationManagement.Infrastructure/IServiceCollectionExtensions.cs
+++ BloodDonationManagement.Infrastructure/IServiceCollectionExtensions.cs
@@ -4,6 +4,10 @@ public static class IServiceCollectionExtensions
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddScoped<IDonatorRepository, DonatorRepository>();
+        services.AddScoped<IBloodStockRepository, BloodStockRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        
         services.AddDbContext<BloodDonationManagementDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
         );
