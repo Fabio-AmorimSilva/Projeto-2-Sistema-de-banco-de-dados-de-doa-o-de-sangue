1ab75b00250d2fa8c833a685ca0ba7e1eacc1432
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-07 19:53:30 -0300

    feat: Creating fail fast validation for commands in application layer

--- BloodDonationManagement.Application/ServiceCollectionExtensions.cs
+++ BloodDonationManagement.Application/ServiceCollectionExtensions.cs
@@ -1,12 +1,13 @@
-﻿namespace BloodDonationManagement.Application;
+﻿namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.TryAddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-        
+
         return services;
     }
 }
