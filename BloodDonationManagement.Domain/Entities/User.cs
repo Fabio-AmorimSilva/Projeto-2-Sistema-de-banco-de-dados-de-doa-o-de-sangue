fb93c4b515bbdd089a5914b4cd7b2f55b6f90a32
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-08 20:04:24 -0300

    feat: Adding new user class and extension for the same class

---
+++ BloodDonationManagement.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿namespace BloodDonationManagement.Domain.Entities;
+
+public class User : Entity
+{
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+
+    public User(
+        string name, 
+        string email, 
+        string password
+    )
+    {
+        Name = name;
+        Email = email;
+        Password = password;
+    }
+}
---
+++ BloodDonationManagement.Domain/Extensions/UserExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace BloodDonationManagement.Domain.Entities;
+
+public static class UserExtensions
+{
+    public static IEnumerable<Claim> GetClaims(this User user)
+    {
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Sid, user.Id.ToString()),
+            new(ClaimTypes.Name, user.Email)
+        };
+
+        return claims;
+    }
+}
