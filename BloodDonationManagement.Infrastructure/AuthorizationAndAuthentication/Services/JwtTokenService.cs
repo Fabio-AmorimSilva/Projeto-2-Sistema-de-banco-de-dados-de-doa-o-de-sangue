dc3633f6f6be69d638aec075fdef8c79c30e7392
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-08 20:05:00 -0300

    feat: Adding new Token Service for generate new Jwt tokens

---
+++ BloodDonationManagement.Infrastructure/AuthorizationAndAuthentication/Services/JwtTokenService.cs
@@ -0,0 +1,28 @@
+﻿namespace BloodDonationManagement.Infrastructure.AuthorizationAndAuthentication.Services;
+
+public class JwtTokenService
+{
+    public class TokenService(IOptions<JwtSettings> settings)
+    {
+        private readonly JwtSettings _jwtSettings = settings.Value;
+
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.JwtKey);
+            var claims = user.GetClaims();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddMinutes(_jwtSettings.ExpireMinutes),
+                Issuer = _jwtSettings.Emissary,
+                Audience = _jwtSettings.ValidOn,
+                SigningCredentials = new SigningCredentials(
+                    key: new SymmetricSecurityKey(key),
+                    algorithm: SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
