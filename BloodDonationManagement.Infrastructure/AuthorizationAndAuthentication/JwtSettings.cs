4535c5ba315717e2c23ada5b2638d947346cf8a0
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-08 20:04:38 -0300

    feat: Creating new class for jwt settings

---
+++ BloodDonationManagement.Infrastructure/AuthorizationAndAuthentication/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BloodDonationManagement.Infrastructure.AuthorizationAndAuthentication;
+
+public class JwtSettings
+{
+    public string? JwtKey { get; init; } = string.Empty;
+    public double ExpireMinutes { get; init; }
+    public string Emissary { get; init; } = string.Empty;
+    public string ValidOn { get; init; } = string.Empty;
+}
