5d48983a2723250383888d59f183ef82845cb084
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-09 20:08:39 -0300

    chore: Adding new namespaces

--- BloodDonationManagement.WebApi/GlobalUsings.cs
+++ BloodDonationManagement.WebApi/GlobalUsings.cs
@@ -3,3 +3,6 @@ global using BloodDonationManagement.Infrastructure.AuthorizationAndAuthenticati
 global using BloodDonationManagement.Infrastructure.AuthorizationAndAuthentication.Services;
 global using BloodDonationManagement.WebApi.ExceptionHandling;
 global using Microsoft.AspNetCore.Diagnostics;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Mvc;
+global using Scalar.AspNetCore;
