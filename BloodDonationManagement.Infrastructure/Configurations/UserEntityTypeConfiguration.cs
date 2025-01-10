d9fff3d2b91ff6684718258d4a6cc760854925f6
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-10 19:54:52 -0300

    feat: Creating configuration for User entity

---
+++ BloodDonationManagement.Infrastructure/Configurations/UserEntityTypeConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace BloodDonationManagement.Infrastructure.Configurations;
+
+public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder
+            .ToTable("Users");
+
+        builder
+            .HasKey(x => x.Id);
+
+        builder
+            .Property(x => x.Id)
+            .IsRequired();
+
+        builder
+            .Property(x => x.Name)
+            .IsRequired();
+
+        builder
+            .Property(x => x.Email)
+            .IsRequired();
+
+        builder
+            .Property(x => x.Password)
+            .IsRequired();
+    }
+}
