da18bcc704d4969944adb431d02c23b39e68ab1b
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-10 19:53:37 -0300

    feat: Creating BloodStocksController and implementing BloodStockReport method

---
+++ BloodDonationManagement.WebApi/Controllers/BloodStocksController.cs
@@ -0,0 +1,14 @@
+﻿namespace BloodDonationManagement.WebApi.Controllers;
+
+[ApiController]
+[Route("api/blood-stocks")]
+public class BloodStocksController(IMediator mediator) : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResultDto<IEnumerable<BloodStockDto>>))]
+    public async Task<ActionResult<IEnumerable<BloodStockDto>>> BloodStockReport()
+    {
+        var bloodStock = await mediator.Send(new BloodStockReportQuery());
+        return Ok(bloodStock.Data);
+    }
+}
