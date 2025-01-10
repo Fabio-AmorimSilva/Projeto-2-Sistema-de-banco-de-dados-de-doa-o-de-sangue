917865fadcd047eadb27c01b370fcb0efe92ef33
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-10 19:53:00 -0300

    feat: Creating IUserRepository interface and implementing methods in UserRepository

---
+++ BloodDonationManagement.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace BloodDonationManagement.Domain.Repositories;
+
+public interface IUserRepository
+{
+    Task CreateAsync(User user);
+    void UpdateAsync(User user);
+    void DeleteAsync(User user);
+}
---
+++ BloodDonationManagement.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace BloodDonationManagement.Infrastructure.Repositories;
+
+public class UserRepository(BloodDonationManagementDbContext context) : IUserRepository
+{
+    public async Task CreateAsync(User user)
+    {
+        await context.Users.AddAsync(user);
+    }
+
+    public async void UpdateAsync(User user)
+    {
+        context.Users.Update(user);
+    }
+
+    public void DeleteAsync(User user)
+    {
+        context.Users.Remove(user);
+    }
+}
