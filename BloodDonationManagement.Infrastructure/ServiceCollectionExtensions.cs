740ecad999fec823cc2777729e747b8081161da9
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-12 18:26:47 -0300

    feat: Adding jwt config service collection method

--- BloodDonationManagement.Infrastructure/ServiceCollectionExtensions.cs
+++ BloodDonationManagement.Infrastructure/ServiceCollectionExtensions.cs
@@ -8,10 +8,43 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IBloodStockRepository, BloodStockRepository>();
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddScoped<PasswordHashService>();
+        services.AddScoped<JwtTokenService>();
         
         services.AddDbContext<BloodDonationManagementDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
         );
+
+        return services;
+    }
+    
+    public static IServiceCollection AddJwtConfig(this IServiceCollection services, IConfiguration configuration)
+    {
+        var settings = configuration.GetSection("JwtSettings");
+        services.Configure<JwtSettings>(settings);
+
+        var appSettings = settings.Get<JwtSettings>();
+        var key = Encoding.ASCII.GetBytes(appSettings?.JwtKey!);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.RequireHttpsMetadata = false;
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = appSettings!.ValidOn,
+                ValidIssuer = appSettings.Emissary
+            };
+        });
 
         return services;
     }
