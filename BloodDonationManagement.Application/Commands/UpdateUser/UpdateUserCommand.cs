b78293a0f3a6a33d3453a8d40c0cdaa3553c5c59
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-12 18:24:41 -0300

    feat: Adding validation in UpdateUserCommand and implementing UpdateUserCommandHandler

--- BloodDonationManagement.Application/Commands/UpdateUser/UpdateUserCommand.cs
+++ BloodDonationManagement.Application/Commands/UpdateUser/UpdateUserCommand.cs
@@ -1,20 +1,26 @@
 ﻿namespace BloodDonationManagement.Application.Commands.UpdateUser;
 
 public record UpdateUserCommand(
+    Guid Id,
     string Name,
     string Email
-) : IRequest;
+) : IRequest<ResultDto<string>>;
 
 public class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
 {
     public UpdateUserCommandValidator()
     {
+        RuleFor(command => command.Id)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateUserCommand.Id)));
+        
         RuleFor(command => command.Name)
             .NotEmpty()
             .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateUserCommand.Name)));
-
+        
         RuleFor(command => command.Email)
             .NotEmpty()
+            .EmailAddress()
             .WithMessage(ErrorMessages.CannotBeEmpty(nameof(UpdateUserCommand.Email)));
     }
 }
--- BloodDonationManagement.Application/Commands/UpdateUser/UpdateUserCommandHandler.cs
+++ BloodDonationManagement.Application/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -1,9 +1,25 @@
 ﻿namespace BloodDonationManagement.Application.Commands.UpdateUser;
 
-public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand>
+public class UpdateUserCommandHandler(
+    IUserRepository repository
+) : IRequestHandler<UpdateUserCommand, ResultDto<string>>
 {
-    public Task Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    public async Task<ResultDto<string>> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var user = await repository.GetAsync(
+            id: request.Id
+        );
+        
+        if(user is null)
+            return ResultDto<string>.Error(ErrorMessages.NotFound<User>());
+
+        user.Update(
+            name: request.Name,
+            email: request.Email
+        );
+        
+        await repository.UpdateAsync(user);
+        
+        return ResultDto<string>.Success();
     }
 }
