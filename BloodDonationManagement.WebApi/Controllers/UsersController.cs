9fc25819ed8b91f7ac1cb6c6ee07caca663671f8
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-12 18:18:49 -0300

    feat: Adding methods in UsersController

---
+++ BloodDonationManagement.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿namespace BloodDonationManagement.WebApi.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/users")]
+public class UsersController(IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    [AllowAnonymous]
+    public async Task<ActionResult> Post([FromBody] InsertUserCommand command)
+    {
+        await mediator.Send(command);
+        return Created();
+    }
+
+    [HttpPut("{userId:guid}")]
+    public async Task<ActionResult> Update([FromBody] UpdateUserCommand command)
+    {
+        var user = await mediator.Send(command);
+        return Ok(user);
+    }
+
+    [HttpPut("{userId:guid}/update-password")]
+    public async Task<ActionResult> Login([FromBody] UpdateUserPasswordCommand command)
+    {
+        await mediator.Send(command);
+        return NoContent();
+    }
+    
+    [HttpPost("login")]
+    [AllowAnonymous]
+    public async Task<ActionResult> Login([FromBody] LoginUserCommand command)
+    {
+        var token = await mediator.Send(command);
+        return Ok(token.Data);
+    }
+}
