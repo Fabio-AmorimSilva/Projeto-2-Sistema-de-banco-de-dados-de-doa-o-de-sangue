c1ef831749a8383631784af20cc98b51a4f2afba
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-12 18:19:14 -0300

    feat: Adding LoginCommand and Implementing LoginUserCommandHandler

---
+++ BloodDonationManagement.Application/Commands/LoginUser/LoginUserCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace BloodDonationManagement.Application.Commands.LoginUser;
+
+public record LoginUserCommand(
+    string Email, 
+    string Password
+) : IRequest<ResultDto<string>>;
+
+public class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
+{
+    public LoginUserCommandValidator()
+    {
+        RuleFor(command => command.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(LoginUserCommand.Email)));
+        
+        RuleFor(command => command.Password)
+            .NotEmpty()
+            .WithMessage(ErrorMessages.CannotBeEmpty(nameof(LoginUserCommand.Password)));
+    }
+}
---
+++ BloodDonationManagement.Application/Commands/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace BloodDonationManagement.Application.Commands.LoginUser;
+
+public class LoginUserCommandHandler(
+    IUserRepository repository,
+    JwtTokenService tokenService
+) : IRequestHandler<LoginUserCommand, ResultDto<string>>
+{
+    public async Task<ResultDto<string>> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+    {
+        var hashedPassword = PasswordHashService.HashPassword(request.Password);
+        var user = await repository.GetUserByEmailAndPassword(request.Email, hashedPassword);
+
+        if (user is null)
+            return ResultDto<string>.Error(ErrorMessages.NotFound<User>());
+
+        var token = tokenService.GenerateToken(user);
+
+        return new ResultDto<string>(token);
+    }
+}
