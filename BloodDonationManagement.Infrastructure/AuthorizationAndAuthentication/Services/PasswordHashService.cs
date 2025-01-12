bd29c4148451d2bc0040b8fc7ebb92db6d5bec9c
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-12 18:26:26 -0300

    feat: Creating hashpassword service

---
+++ BloodDonationManagement.Infrastructure/AuthorizationAndAuthentication/Services/PasswordHashService.cs
@@ -0,0 +1,22 @@
+﻿namespace BloodDonationManagement.Infrastructure.AuthorizationAndAuthentication.Services;
+
+public class PasswordHashService
+{
+    public static string HashPassword(string password)
+    {
+        string combinedPassword = password;
+
+        using (var sha256 = SHA256.Create())
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(combinedPassword);
+
+            byte[] hash = sha256.ComputeHash(bytes);
+
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+                result.Append(hash[i].ToString("x2"));
+
+            return result.ToString();
+        }
+    }
+}
