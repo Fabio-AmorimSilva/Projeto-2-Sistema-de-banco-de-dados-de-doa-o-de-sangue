d1c8bbf3e2200110998726469b5022b283749c18
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-12 18:20:00 -0300

    refactor: Correcting service implementation

--- BloodDonationManagement.Infrastructure/AuthorizationAndAuthentication/Services/JwtTokenService.cs
+++ BloodDonationManagement.Infrastructure/AuthorizationAndAuthentication/Services/JwtTokenService.cs
@@ -1,28 +1,25 @@
 ﻿namespace BloodDonationManagement.Infrastructure.AuthorizationAndAuthentication.Services;
 
-public class JwtTokenService
+public class JwtTokenService(IOptions<JwtSettings> settings)
 {
-    public class TokenService(IOptions<JwtSettings> settings)
-    {
-        private readonly JwtSettings _jwtSettings = settings.Value;
+    private readonly JwtSettings _jwtSettings = settings.Value;
 
-        public string GenerateToken(User user)
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_jwtSettings.JwtKey!);
+        var claims = user.GetClaims();
+        var tokenDescriptor = new SecurityTokenDescriptor
         {
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_jwtSettings.JwtKey);
-            var claims = user.GetClaims();
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.UtcNow.AddMinutes(_jwtSettings.ExpireMinutes),
-                Issuer = _jwtSettings.Emissary,
-                Audience = _jwtSettings.ValidOn,
-                SigningCredentials = new SigningCredentials(
-                    key: new SymmetricSecurityKey(key),
-                    algorithm: SecurityAlgorithms.HmacSha256Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            return tokenHandler.WriteToken(token);
-        }
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddMinutes(_jwtSettings.ExpireMinutes),
+            Issuer = _jwtSettings.Emissary,
+            Audience = _jwtSettings.ValidOn,
+            SigningCredentials = new SigningCredentials(
+                key: new SymmetricSecurityKey(key),
+                algorithm: SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
     }
 }
