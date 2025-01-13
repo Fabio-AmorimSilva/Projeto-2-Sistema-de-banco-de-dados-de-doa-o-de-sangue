b6a94384abf2476c7e5fa6596bbf97bfa6cdb2a4
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-13 20:24:32 -0300

    feat: Adding new migration for load data in database

---
+++ BloodDonationManagement.Infrastructure/Migrations/20250113221938_AddingBloodTypesInDatabase.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BloodDonationManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingBloodTypesInDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"""
+                  INSERT INTO BloodStocks(Id, BloodType, RhFactor, Quantity)
+                  VALUES(NEWID(), 1, 1, 0);
+
+                  INSERT INTO BloodStocks(Id, BloodType, RhFactor, Quantity)
+                  VALUES(NEWID(), 1, 2, 0);
+
+                  INSERT INTO BloodStocks(Id, BloodType, RhFactor, Quantity)
+                  VALUES(NEWID(), 2, 1, 0);
+
+                  INSERT INTO BloodStocks(Id, BloodType, RhFactor, Quantity)
+                  VALUES(NEWID(), 2, 2, 0);
+
+                  INSERT INTO BloodStocks(Id, BloodType, RhFactor, Quantity)
+                  VALUES(NEWID(), 3, 1, 0);
+
+                  INSERT INTO BloodStocks(Id, BloodType, RhFactor, Quantity)
+                  VALUES(NEWID(), 3, 2, 0);
+
+                  INSERT INTO BloodStocks(Id, BloodType, RhFactor, Quantity)
+                  VALUES(NEWID(), 4, 1, 0);
+
+                  INSERT INTO BloodStocks(Id, BloodType, RhFactor, Quantity)
+                  VALUES(NEWID(), 4, 2, 0);
+              """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
