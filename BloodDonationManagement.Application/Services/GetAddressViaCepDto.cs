65f9ca0452b2f66eb00b968733978c26fc791961
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-17 19:15:04 -0300

    refactor: Removing unused properties

--- BloodDonationManagement.Application/Services/GetAddressViaCepDto.cs
+++ BloodDonationManagement.Application/Services/GetAddressViaCepDto.cs
@@ -4,15 +4,6 @@ public record GetAddressViaCepDto
 {
     public string Cep { get; init; } = null!;
     public string Logradouro { get; init; } = null!;
-    public string Complemento { get; init; } = null!;
-    public string Unidade { get; init; } = null!;
-    public string Bairro { get; init; } = null!;
     public string Localidade { get; init; } = null!;
-    public string Uf { get; init; } = null!;
     public string Estado { get; init; } = null!;
-    public string Regiao { get; init; } = null!;
-    public string Ibge { get; init; } = null!;
-    public string Gia { get; init; } = null!;
-    public string DDD { get; init; } = null!;
-    public string Siafi { get; init; } = null!;
 }
