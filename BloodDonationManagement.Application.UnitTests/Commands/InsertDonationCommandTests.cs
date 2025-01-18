28bb5502b139281fbd108851a567a79fe6129db5
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-18 16:55:48 -0300

    feat: Adding validations for commands

---
+++ BloodDonationManagement.Application.UnitTests/Commands/InsertDonationCommandTests.cs
@@ -0,0 +1,48 @@
+﻿namespace BloodDonationManagement.Application.Tests.Commands;
+
+public class InsertDonationCommandTests
+{
+    [Theory]
+    [MemberData(nameof(SimulationInsertDonationCommand))]
+    public void ShouldReturnErrorWhenValidationFails(InsertDonationCommand command,
+        string expectedErrorMessage
+    )
+    {
+        var errorMessage = new InsertDonationCommandValidator().Validate(command);
+
+        errorMessage.IsValid.Should().BeFalse();
+        errorMessage.Errors.Should().HaveCount(1);
+        errorMessage.Errors[0].ErrorMessage.Should().Be(expectedErrorMessage);
+    }
+
+    public static TheoryData<InsertDonationCommand, string> SimulationInsertDonationCommand()
+    {
+        return new TheoryData<InsertDonationCommand, string>
+        {
+            {
+                new InsertDonationCommand(
+                    DonorId: Guid.Empty,
+                    DonationDate: DateTime.Now,
+                    Quantity: 1
+                ),
+                ErrorMessages.CannotBeEmpty(nameof(InsertDonationCommand.DonorId))
+            },
+            {
+                new InsertDonationCommand(
+                    DonorId: Guid.NewGuid(),
+                    DonationDate: DateTime.MinValue,
+                    Quantity: 1
+                ),
+                ErrorMessages.CannotBeEmpty(nameof(InsertDonationCommand.DonationDate))
+            },
+            {
+                new InsertDonationCommand(
+                    DonorId: Guid.NewGuid(),
+                    DonationDate: DateTime.Now,
+                    Quantity: 0
+                ),
+                ErrorMessages.CannotBeEmpty(nameof(InsertDonationCommand.Quantity))
+            }
+        };
+    }
+}
---
+++ BloodDonationManagement.Application.UnitTests/Commands/InsertDonorCommandTests.cs
@@ -0,0 +1,128 @@
+﻿namespace BloodDonationManagement.Application.Tests.Commands;
+
+public class InsertDonorCommandTests
+{
+    [Theory]
+    [MemberData(nameof(SimulationInsertDonationCommand))]
+    public void ShouldReturnErrorWhenValidationFails(InsertDonorCommand command,
+        string expectedErrorMessage
+    )
+    {
+        var errorMessage = new InsertDonorCommandValidator().Validate(command);
+
+        errorMessage.IsValid.Should().BeFalse();
+        errorMessage.Errors.Should().HaveCount(1);
+        errorMessage.Errors[0].ErrorMessage.Should().Be(expectedErrorMessage);
+    }
+
+    public static TheoryData<InsertDonorCommand, string> SimulationInsertDonationCommand()
+    {
+        return new TheoryData<InsertDonorCommand, string>
+        {
+            {
+                new InsertDonorCommand(
+                    Name: string.Empty,
+                    Email: "teste@teste.com",
+                    Birth: DateTime.Now,
+                    Gender: Gender.Male,
+                    Weight: 1,
+                    BloodType: BloodType.A,
+                    RhFactor: RhFactor.Negative,
+                    Cep: "000000"
+                ),
+                ErrorMessages.CannotBeEmpty(nameof(InsertDonorCommand.Name))
+            },
+            {
+                new InsertDonorCommand(
+                    Name: "teste",
+                    Email: string.Empty,
+                    Birth: DateTime.Now,
+                    Gender: Gender.Male,
+                    Weight: 1,
+                    BloodType: BloodType.A,
+                    RhFactor: RhFactor.Negative,
+                    Cep: "000000"
+                ),
+                ErrorMessages.CannotBeEmpty(nameof(InsertDonorCommand.Email))
+            },
+            {
+                new InsertDonorCommand(
+                    Name: "teste",
+                    Email: "teste@teste.com",
+                    Birth: DateTime.MinValue,
+                    Gender: Gender.Male,
+                    Weight: 1,
+                    BloodType: BloodType.A,
+                    RhFactor: RhFactor.Negative,
+                    Cep: "000000"
+                ),
+                ErrorMessages.CannotBeEmpty(nameof(InsertDonorCommand.Birth))
+            },
+            {
+                new InsertDonorCommand(
+                    Name: "teste",
+                    Email: "teste@teste.com",
+                    Birth: DateTime.Now,
+                    Gender: (Gender)3,
+                    Weight: 1,
+                    BloodType: BloodType.A,
+                    RhFactor: RhFactor.Negative,
+                    Cep: "000000"
+                ),
+                ErrorMessages.CannotBeEmpty(nameof(InsertDonorCommand.Gender))
+            },
+            {
+                new InsertDonorCommand(
+                    Name: "teste",
+                    Email: "teste@teste.com",
+                    Birth: DateTime.Now,
+                    Gender: Gender.Male,
+                    Weight: 0,
+                    BloodType: BloodType.A,
+                    RhFactor: RhFactor.Negative,
+                    Cep: "000000"
+                ),
+                ErrorMessages.CannotBeEmpty(nameof(InsertDonorCommand.Weight))
+            },
+            {
+                new InsertDonorCommand(
+                    Name: "teste",
+                    Email: "teste@teste.com",
+                    Birth: DateTime.Now,
+                    Gender: Gender.Male,
+                    Weight: 1,
+                    BloodType: (BloodType)10,
+                    RhFactor: RhFactor.Negative,
+                    Cep: "000000"
+                ),
+                ErrorMessages.CannotBeEmpty(nameof(InsertDonorCommand.BloodType))
+            },
+            {
+                new InsertDonorCommand(
+                    Name: "teste",
+                    Email: "teste@teste.com",
+                    Birth: DateTime.Now,
+                    Gender: Gender.Male,
+                    Weight: 1,
+                    BloodType: BloodType.A,
+                    RhFactor: (RhFactor)10,
+                    Cep: "000000"
+                ),
+                ErrorMessages.CannotBeEmpty(nameof(InsertDonorCommand.RhFactor))
+            },
+            {
+                new InsertDonorCommand(
+                    Name: "teste",
+                    Email: "teste@teste.com",
+                    Birth: DateTime.Now,
+                    Gender: Gender.Male,
+                    Weight: 1,
+                    BloodType: BloodType.A,
+                    RhFactor: RhFactor.Negative,
+                    Cep: string.Empty
+                ),
+                ErrorMessages.CannotBeEmpty(nameof(InsertDonorCommand.Cep))
+            }
+        };
+    }
+}
