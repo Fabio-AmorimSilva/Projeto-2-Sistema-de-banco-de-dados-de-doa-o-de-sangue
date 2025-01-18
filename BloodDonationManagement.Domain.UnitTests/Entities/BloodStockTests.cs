45983794b73bc03b965095c72c287c100fc1c372
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-01-18 16:56:48 -0300

    feat: Adding validation for BloodStock

---
+++ BloodDonationManagement.Domain.UnitTests/Entities/BloodStockTests.cs
@@ -0,0 +1,17 @@
+﻿namespace BloodDonationManagement.Domain.Tests.Entities;
+
+public class BloodStockTests
+{
+    [Fact]
+    public void ShouldThrowArgumentNullExceptionWhenBloodStockQuantityIsDefault()
+    {
+        var action = () => new BloodStock(
+            bloodType: BloodType.A, 
+            rhFactor: RhFactor.Negative, 
+            quantity: 0
+        );
+
+        action.Should().Throw<ArgumentException>()
+            .WithParameterName("quantity");
+    }
+}
