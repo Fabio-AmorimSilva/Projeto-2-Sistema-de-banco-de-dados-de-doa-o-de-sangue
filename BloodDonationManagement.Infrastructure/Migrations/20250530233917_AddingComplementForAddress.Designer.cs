5d7cefb477dfcef6d6363335d423fd76de43673c
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2025-05-30 21:19:14 -0300

    feat: Adding new migration

--- BloodDonationManagement.Infrastructure/Migrations/20250530233917_AddingComplementForAddress.Designer.cs
+++ BloodDonationManagement.Infrastructure/Migrations/20250530233917_AddingComplementForAddress.Designer.cs
@@ -4,6 +4,7 @@ using BloodDonationManagement.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BloodDonationManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(BloodDonationManagementDbContext))]
-    partial class BloodDonationManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250530233917_AddingComplementForAddress")]
+    partial class AddingComplementForAddress
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BloodDonationManagement.Infrastructure/Migrations/20250530233917_AddingComplementForAddress.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BloodDonationManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingComplementForAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address_Complement",
+                table: "Donors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address_Complement",
+                table: "Donors");
+        }
+    }
+}
